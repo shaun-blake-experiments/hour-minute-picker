af3e8fc4f0e237ea48c6eee8e9aee71c2f7bd742
Shaun Blake <sfblake@hawaii.edu>
2015-01-21 14:20:45 -1000

    Clean up a little and prepare to launch start using the picker.

--- sample-date-measure-app/src/main/java/edu/hawaii/android/sampledatemeasureapp/MainActivity.java
+++ sample-date-measure-app/src/main/java/edu/hawaii/android/sampledatemeasureapp/MainActivity.java
@@ -1,15 +1,12 @@
 package edu.hawaii.android.sampledatemeasureapp;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -25,29 +22,6 @@ public class MainActivity extends ActionBarActivity {
         }
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     /**
      * A placeholder fragment containing a simple view.
      */
@@ -60,6 +34,20 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            View datePickerBtn = rootView.findViewById(R.id.showTimeMeasurePicker);
+            datePickerBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    CharSequence text = "This is where I would use the time measure picker...";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(PlaceholderFragment.this.getActivity(), text, duration);
+                    toast.show();
+                }
+            });
+
             return rootView;
         }
     }
